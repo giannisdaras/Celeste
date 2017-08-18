489c4abfe9b955722887d4852e8fd865cd40f779
John <daras.giannhs@gmail.com>
2017-08-18 23:55:14 +0300

    bug fixes to controller.py

--- controller.py
+++ controller.py
@@ -1,23 +1,28 @@
 import speech_recognition as sr
-homeName="smart"
-while(True):
-	recognizer = sr.Recognizer()
-	try:
-		with sr.Microphone() as source:
-		    recognizer.adjust_for_ambient_noise(source)
-		    message=recognizer.recognize_google(audio1)
-		    if (homeName in message):
-		    	text=message[message.index(homeName) + len(homeName):]
-		    	if ("training" in text):
-		    		train(text)
-		    	else:
-		    		changeHomeState(text)
-	except:
-		print("Check your connection")
 
 def train(command):
+	print(text)
 	return
 
 def changeHomeState(command):
 	return
+
+homeName="home"
+recognizer = sr.Recognizer()
+while(True):
+	with sr.Microphone() as source:
+	    recognizer.adjust_for_ambient_noise(source)
+	    recognizer.energy_threshold=500
+	    audio1 = recognizer.listen(source)
+	    try:
+	    	message=recognizer.recognize_google(audio1)
+	    	print(message)
+	    	text=message[message.index(homeName) + len(homeName) + 1:]
+	    	if ("training" in text):
+	    		train(text)
+	    except sr.UnknownValueError:
+	    	print("did not hear")
+	
+
+
 
