9cdb2af03885e26c121a78c58c370659eb173cc9
John <daras.giannhs@gmail.com>
2017-08-18 17:37:39 +0300

    This commit modifies speech recognition script in order to store the command for further use, after the smart home has been voice called from the user

--- sp_re.py
+++ sp_re.py
@@ -1,24 +1,15 @@
 import speech_recognition as sr
-
-r1 = sr.Recognizer()
-with sr.Microphone() as source:
-    r1.adjust_for_ambient_noise(source)  # listen for 1 second to calibrate the energy threshold for ambient noise levels
-    print("Say something!")
-    audio1 = r1.listen(source)
-
-if "ok" in r1.recognize_google(audio1):
-	r2 = sr.Recognizer()
-	with sr.Microphone() as source:
-		r2.adjust_for_ambient_noise(source)  # listen for 1 second to calibrate the energy threshold for ambient noise levels 
-		print("Now give an order!")
-		audio2 = r2.listen(source)
+homeName="smart"
+while(True):
+	recognizer = sr.Recognizer()
 	try:
-   		print("You probably said: " + r2.recognize_google(audio2))
-	except sr.UnknownValueError:
-   		print("Sorry, i didn't quite catch that")
-	except sr.RequestError as e:
-		print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-else:
-	print("No response")
+		with sr.Microphone() as source:
+		    recognizer.adjust_for_ambient_noise(source)
+		    message=recognizer.recognize_google(audio1)
+			if homeName in message:
+				command=message[message.index(homeName) + len(homeName):]
+				print("I listened the command:" + command)
+	except:
+		print("Check your connection and try again")
+		continue
 	
