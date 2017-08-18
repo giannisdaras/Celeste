2053014477d7cfd4ff39fb23e01b819415a2ca72
John <daras.giannhs@gmail.com>
2017-08-19 00:59:56 +0300

    PySerial added to controller for arduino communication

--- controller.py
+++ controller.py
@@ -1,27 +1,34 @@
 import speech_recognition as sr
+import serial
 
 def train(command):
-	print(text)
+	print("Training")
 	return
 
 def changeHomeState(command):
 	return
+
+def arduino(command):
+	#replace with your own port
+	ser = serial.Serial('/dev/ttyACM1', 9600)
+	ser.write(command)
+	return
+	
 
 homeName="home"
 recognizer = sr.Recognizer()
 while(True):
 	with sr.Microphone() as source:
 	    recognizer.adjust_for_ambient_noise(source)
-	    recognizer.energy_threshold=500
 	    audio1 = recognizer.listen(source)
 	    try:
 	    	message=recognizer.recognize_google(audio1)
-	    	print(message)
-	    	text=message[message.index(homeName) + len(homeName) + 1:]
-	    	if ("training" in text):
-	    		train(text)
+	    	if (homeName in message):
+		    	text=message[message.index(homeName) + len(homeName) + 1:]
+		    	if ("training" in text):
+		    		train(text)
 	    except sr.UnknownValueError:
-	    	print("did not hear")
+	    	print("Untracked sound")
 	
 
 
