9701e5c06206e93aeb4b0f11427365cef3122db6
John <daras.giannhs@gmail.com>
2017-08-18 18:03:55 +0300

    indendation fix

--- controller.py
+++ controller.py
@@ -6,10 +6,8 @@ while(True):
 		with sr.Microphone() as source:
 		    recognizer.adjust_for_ambient_noise(source)
 		    message=recognizer.recognize_google(audio1)
-			if homeName in message:
-				command=message[message.index(homeName) + len(homeName):]
-				print("I listened the command:" + command)
+		    if (homeName in message):
+		    	command=message[message.index(homeName) + len(homeName):]
 	except:
-		print("Check your connection and try again")
-		continue
+		print("Check your connection")
 	
