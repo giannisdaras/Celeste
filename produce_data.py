cf2140440bd839bd7329b09d8e13930b2a7d7f80
John <daras.giannhs@gmail.com>
2017-08-20 03:08:17 +0300

    this commit deletes useless libraries declarations

--- produce_data.py
+++ produce_data.py
@@ -1,23 +1,5 @@
 import time
 import numpy as np
-import pandas as pd 
-from sklearn.preprocessing import StandardScaler
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import preprocessing
-from sklearn.model_selection import cross_val_score
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
-from sklearn.model_selection import GridSearchCV
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.neural_network import MLPClassifier
-from sklearn import preprocessing
-from sklearn.naive_bayes import GaussianNB
-from sklearn.decomposition import PCA
-from sklearn.ensemble import VotingClassifier
-from sklearn.metrics import f1_score
-
 
 file1=open('training_data.csv','w')
 file2=open('training_results.csv','w')
