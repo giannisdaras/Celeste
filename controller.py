40daf10100183e56703af6df043896ac09a7040d
John <daras.giannhs@gmail.com>
2017-08-21 15:03:37 +0300

    Funny team name generator added. Efforts for formality to controller.py

--- controller.py
+++ controller.py
@@ -4,35 +4,39 @@ import threading
 import time
 import numpy as np
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import preprocessing
-from sklearn.model_selection import cross_val_score
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
-from sklearn.model_selection import GridSearchCV
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.neural_network import MLPClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.decomposition import PCA
-from sklearn.ensemble import VotingClassifier
-from sklearn.metrics import f1_score
-
+global homeName
+homeName="homeName"
 
 def fire():
-	fold_numbers=4
-	X = np.genfromtxt('training_data.csv', delimiter=',')
-	y = np.genfromtxt('training_results.csv', delimiter=',')
-	forest=RandomForestClassifier(criterion='entropy',n_estimators=31,n_jobs=1)
-	forest.fit(X,y)
-	print(forest.predict([10,01,0,0,28]))
+	return
 
-class foo(threading.Thread):
-	#basic thread function!
+class VoiceRecognition():
+	def __init__(self):
+		self.recognizer = sr.Recognizer()
+		return
+	def rec(self):
+		while(True):
+			with sr.Microphone() as source:
+			    self.recognizer.adjust_for_ambient_noise(source)
+			    audio1 = self.recognizer.listen(source)
+			    try:
+			    	message=self.recognizer.recognize_google(audio1)
+			    	if (homeName in message):
+				    	text=message[message.index(homeName) + len(homeName) + 1:]
+				    	if ("training" in text):
+				    		train(text)
+			    except sr.UnknownValueError:
+			    	print("Untracked sound")
+			    except:
+			    	print("No internet connection")
+			    	break
+		return
+		
+
+class Event(threading.Thread):
 	def run(self):
 		time.sleep(2)
+		print('something')
 		eventFlag=checkForEvent()
 		if (eventFlag==1):
 			eventFlag=0
@@ -67,22 +71,8 @@ def changeHomeState(command):
 	return
 
 
-homeName="home"
-recognizer = sr.Recognizer()
-while(True):
-	with sr.Microphone() as source:
-	    recognizer.adjust_for_ambient_noise(source)
-	    audio1 = recognizer.listen(source)
-	    try:
-	    	message=recognizer.recognize_google(audio1)
-	    	if (homeName in message):
-		    	text=message[message.index(homeName) + len(homeName) + 1:]
-		    	if ("training" in text):
-		    		train(text)
-	    except sr.UnknownValueError:
-	    	print("Untracked sound")
-	    except:
-	    	print("No internet connection")
-	    	break
-fire()
+event= Event()
+
+
+
 
