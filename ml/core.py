01bd61a8ffec275761dc90231c332ee37a7c5b0e
papachristoumarios <papachristoumarios@gmail.com>
2017-08-21 20:36:13 +0300

    added gitignore and made minor adjustments

--- ml/core.py
+++ ml/core.py
@@ -1,8 +1,10 @@
 from __init__ import *
 
+
 def softmax(y):
 	z = np.exp(y)
 	return z / np.sum(z)
+
 
 class State:
 
@@ -25,8 +27,9 @@ class State:
 	def addSubscriber(self, sub):
 		self.subscribers.append(sub)
 
-	def nameSub(self ,x):
+	def nameSub(self, x):
 		print self.name
+
 
 class Sensor:
 
@@ -37,15 +40,18 @@ class Sensor:
 	def getData(self):
 		return None
 
+
 class DummySensor(Sensor):
 
 	def __init__(self, name, output_ports, lower, upper):
-		#super(DummySensor, self).__init__(name, output_ports)
+		# super(DummySensor, self).__init__(name, output_ports)
 		self.name, self.output_ports = name, output_ports
 		self.lower, self.upper = lower, upper
 
 	def getData(self):
-		return np.array(self.output_ports * [np.random.randint(self.lower, self.upper)])
+		return np.array(self.output_ports *
+     [np.random.randint(self.lower, self.upper)])
+
 
 class Camera(Sensor):
 
@@ -64,11 +70,12 @@ class Camera(Sensor):
 	def __del__(self):
 		self.cam.release()
 
+
 class StatePredictor:
 
 	def __init__(self, states, sensors, optimizer='adam'):
 		# Initializations
-		self.states = states #states as dictionary
+		self.states = states  # states as dictionary
 		for k in self.states.keys():
 			self.states[k].stateid = k
 		self.sensors = sensors
@@ -85,7 +92,7 @@ class StatePredictor:
 
 		self.num_classes = len(self.states)
 
-		#Model configuration
+		# Model configuration
 		# TODO add model topology
 		self.model = Sequential()
 		self.model.add(Dense(16, input_dim=self.num_inputs, activation='relu'))
@@ -97,7 +104,6 @@ class StatePredictor:
 		# y1, y2, ym : Next state probabilities
 		self.model.add(Dense(self.num_classes, activation='sigmoid'))
 
-
 		self.model.compile(loss='categorical_crossentropy',
 		optimizer=optimizer,
 		metrics=['accuracy'])
@@ -106,11 +112,13 @@ class StatePredictor:
 		if type(x) == str and type(y) == str:
 			x = np.genfromtxt(x, delimiter=delimiter)
 			y = np.genfromtxt(y, delimiter=delimiter)
+
 		# one-hot encoding
 		if not onehot:
 			y = keras.utils.to_categorical(y, self.num_classes)
 
 		self.model.fit(x, y, epochs=epochs, batch_size=batch_size)
+
 	def saveWeights(self, filename='weights.h5'):
 		self.model.save_weights(filename)
 
@@ -120,7 +128,7 @@ class StatePredictor:
 	def __repr__(self):
 		self.model.summary()
 
-	def update(self):
+	def update(self, retrain=True):
 		dt = time.time() - self.start_time
 		x = np.array([dt, self.state.stateid])
 
@@ -128,11 +136,15 @@ class StatePredictor:
 			np.append(x, sensor.getData().flatten())
 
 		y_hat = self.model.predict(x)
-		#use softmax
+		# use softmax
 		# y_hat = softmax(self.model.predict(x))
 		index = np.argmax(y_hat)
 		self.state = self.states[index]
 		print 'New state: {0}'.format(self.states[index])
+
+		# Retrain our model
+		if retrain == True:
+			self.model.fit(x, y_hat, epochs=1, batch_size=128)
 
 		self.state.OnActivation(x)
 
@@ -143,6 +155,7 @@ class StatePredictor:
 
 		self.scores = self.model.evaluate(x_test, y_test)
 		return self.scores
+
 
 class StatePredictorThread(threading.Thread):
 
@@ -156,28 +169,35 @@ class StatePredictorThread(threading.Thread):
 			self.predictor.update()
 			time.sleep(self.update_interval)
 
-# Unittesting
-def test():
-	sensors = [
-		DummySensor('number_of_people', 1, 0, 20),
-		DummySensor('mood', 1, 0, 10),
-		DummySensor('light', 1, 0, 255),
-		DummySensor('temperature', 1, 10, 40)
-	]
 
-	states = {
-		1 : State('Do Nothing', 1),
-		2 : State('Raise temperature', 2),
-		3 : State('Decrease temperature', 3),
-		4 : State('Turn on music', 4),
-		5 : State('Close shutters', 5),
-	}
+class StatePredictorTestCase(unittest.TestCase):
 
-	state_predictor = StatePredictor(states, sensors)
-	state_predictor.train('train_data_x.csv', 'train_data_y.csv')
-	state_predictor.saveWeights()
+	def test(self):
+		self.sensors = [
+			DummySensor('number_of_people', 1, 0, 20),
+			DummySensor('mood', 1, 0, 10),
+			DummySensor('light', 1, 0, 255),
+			DummySensor('temperature', 1, 10, 40)
+		]
 
-	state_predictor_thread = StatePredictorThread(predictor=state_predictor, update_interval=update_interval)
-    state_predictor_thread.start()
+		self.states = {
+			1: State('Do Nothing', 1),
+			2: State('Raise temperature', 2),
+			3: State('Decrease temperature', 3),
+			4: State('Turn on music', 4),
+			5: State('Close shutters', 5),
+		}
 
-test()
+		self.state_predictor = StatePredictor(states, sensors)
+		self.state_predictor.train('train_data_x.csv', 'train_data_y.csv')
+		self.state_predictor.saveWeights()
+
+		self.state_predictor_thread = StatePredictorThread(
+    predictor=state_predictor, update_interval=update_interval)
+	    state_predictor_thread.start()
+
+	def evaluate(self):
+
+
+if __name__ == '__main__':
+	unittest.main()
