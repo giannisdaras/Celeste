424f82b8c32c89b59a6c76292388133cdd20b889
John <daras.giannhs@gmail.com>
2017-08-21 23:10:30 +0300

    Voice recognition working as thread

--- src/controller.py
+++ src/controller.py
@@ -5,63 +5,42 @@ import time
 import numpy as np
 
 global homeName
-homeName="homeName"
+homeName="home"
 
 def fire():
 	return
 
-class VoiceRecognition():
+class VoiceClassifier():
 	def __init__(self):
 		self.recognizer = sr.Recognizer()
 		return
 	def rec(self):
 		while(True):
+			print(1)
 			with sr.Microphone() as source:
 			    self.recognizer.adjust_for_ambient_noise(source)
 			    audio1 = self.recognizer.listen(source)
 			    try:
 			    	message=self.recognizer.recognize_google(audio1)
+			    	print('message was: ' + message)
 			    	if (homeName in message):
 				    	text=message[message.index(homeName) + len(homeName) + 1:]
 				    	if ("training" in text):
 				    		train(text)
 			    except sr.UnknownValueError:
-			    	print("Untracked sound")
+			    	print('Untracked')
 			    except:
-			    	print("No internet connection")
-			    	break
+			    	continue
 		return
 		
 
-class Event(threading.Thread):
-	def run(self):
-		time.sleep(2)
-		print('something')
-		eventFlag=checkForEvent()
-		if (eventFlag==1):
-			eventFlag=0
-			fire()
-	def checkForEvent(self):
-		return 0
-
-
-class Arduino:
+class VoiceThread(threading.Thread):
 	def __init__(self):
-		x = 0
-		for x in range(10):
-			try:
-				#check if ports work
-				self.ser = serial.Serial('/dev/ttyACM{0}'.format(x), 9600)
-				return
-			except:
-				continue
-		print('No port found')
-
-	def command(self, cmd):
-		self.ser.write(cmd)			
-
-	def __del__(self):
-		self.ser.close()
+		threading.Thread.__init__(self)
+		self.recorder=VoiceClassifier()
+	def run(self):
+		self.recorder.rec()
+		return
 
 def train(command):
 	print("Training")
@@ -70,9 +49,14 @@ def train(command):
 def changeHomeState(command):
 	return
 
+voice_thread=VoiceThread()
+voice_thread.start()
 
-recorder=VoiceRecognition()
-recorder.rec()
+
+
+
+
+
 
 
 
