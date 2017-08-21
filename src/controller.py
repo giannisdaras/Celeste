77b90ae453e9b25b7842a050fd802c7953458378
papachristoumarios <papachristoumarios@gmail.com>
2017-08-22 01:06:59 +0300

    thread functionality altered

--- src/controller.py
+++ src/controller.py
@@ -10,37 +10,40 @@ homeName="home"
 def fire():
 	return
 
-class VoiceClassifier():
-	def __init__(self):
-		self.recognizer = sr.Recognizer()
-		return
-	def rec(self):
-		while(True):
-			print(1)
-			with sr.Microphone() as source:
-			    self.recognizer.adjust_for_ambient_noise(source)
-			    audio1 = self.recognizer.listen(source)
-			    try:
-			    	message=self.recognizer.recognize_google(audio1)
-			    	print('message was: ' + message)
-			    	if (homeName in message):
-				    	text=message[message.index(homeName) + len(homeName) + 1:]
-				    	if ("training" in text):
-				    		train(text)
-			    except sr.UnknownValueError:
-			    	print('Untracked')
-			    except:
-			    	continue
-		return
-		
-
-class VoiceThread(threading.Thread):
+class VoiceClassifier(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
-		self.recorder=VoiceClassifier()
+		self.running = True
+		self.recognizer = sr.Recognizer()
+
+	def rec(self):
+		while True:
+			while self.running:
+				print(1)
+				with sr.Microphone() as source:
+				    self.recognizer.adjust_for_ambient_noise(source)
+				    audio1 = self.recognizer.listen(source)
+				    try:
+				    	message=self.recognizer.recognize_google(audio1)
+				    	print('message was: ' + message)
+				    	if (homeName in message):
+					    	text=message[message.index(homeName) + len(homeName) + 1:]
+					    	if ("training" in text):
+					    		train(text)
+				    except sr.UnknownValueError:
+				    	print('Untracked')
+				    except:
+				    	continue
+
 	def run(self):
-		self.recorder.rec()
-		return
+		self.rec()
+
+	def pause(self):
+		self.running = False
+
+	def resume(self):
+		self.running = True
+
 
 def train(command):
 	print("Training")
@@ -49,15 +52,9 @@ def train(command):
 def changeHomeState(command):
 	return
 
-voice_thread=VoiceThread()
-voice_thread.start()
+def test():
+	voice_classifier = VoiceClassifier()
+	voice_classifier.start()
 
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+	test()
