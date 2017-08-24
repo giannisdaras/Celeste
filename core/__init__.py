c06eb0e35e7f08f1d1a15daa3d8c35fe551bbf65
John <daras.giannhs@gmail.com>
2017-08-24 23:57:16 +0300

    Clones locally master branch

---
+++ core/__init__.py
@@ -0,0 +1,13 @@
+import keras
+import time
+import datetime
+import threading
+import multiprocessing
+import numpy as np
+import cv2
+import unittest
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Convolution2D, MaxPooling2D
+import speech_recognition as sr
+import threading
+from PyMata.pymata import PyMata
---
+++ core/io.py
@@ -0,0 +1,86 @@
+from __init__ import *
+
+def get_board(num_tries = 10):
+    for i in range(num_tries):
+        try:
+            board = PyMata('/dev/ttyACM{0}'.format(i))
+            return board
+        except:
+            continue
+    return None
+
+# Sensing
+
+class Sensor:
+
+    def __init__(self, name, output_ports):
+        self.name = name
+        self.output_ports = output_ports
+
+    def getData(self):
+        return None
+
+
+class DummySensor(Sensor):
+    """ Sensor that outputs random data """
+
+    def __init__(self, name, output_ports, lower, upper):
+        super(DummySensor, self).__init__(name, output_ports)
+        self.lower, self.upper = lower, upper
+
+    def getData(self):
+        return np.array(self.output_ports *
+                        [np.random.randint(self.lower, self.upper)])
+
+
+class Camera(Sensor):
+
+    def __init__(self, name, index):
+        self.cam = cv2.VideoCapture(index)
+        ret, frame = self.cam.read()
+        frame = frame.flatten()
+        super(Camera, self).__init__(name, frame.shape[0])
+
+    def getData(self, grayscale=True):
+        ret, frame = self.cam.read()
+        if grayscale:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        return frame
+
+    def __del__(self):
+        self.cam.release()
+
+class ArduinoPin:
+
+    def __init__(self, pin, mode, pintype):
+        # TODO add property setters and getters
+        self.pin = pin
+        self.mode = mode
+        self.pintype = pintype
+
+class ArduinoSensor(Sensor):
+
+    def __init__(self, name, output_ports, board, input_pins=[]):
+        super(self, ArduinoSensor).__init__(name, output_ports)
+        self.board = board
+        self.input_pins = input_pins
+
+        #setup board
+        for pin in input_pins:
+            self.board.set_pin_mode(pin.pin, self.board.INPUT if pin.mode == 'INPUT' else self.board.PWM,
+            self.board.DIGITAL if pin.pintype=='DIGITAL' else self.board.ANALOG)
+
+    def getData(self):
+        x = np.array([])
+        for pin in self.input_pins:
+            x = np.append(x, self.board.analog_read(pin.pin))
+        return x
+
+# Enums for Arduino
+# TODO Add desired sensors 
+
+class ArduinoSensors:
+    pass
+
+class ArduinoOutputs:
+    pass
