953082821074bc7ecaa802e724a34f289d47e512
papachristoumarios <papachristoumarios@gmail.com>
2017-09-06 15:51:15 +0300

    put in the correct place

--- core/music.py
+++ core/music.py
@@ -4,9 +4,11 @@ import spotipy.util as util
 from os import system as cmd
 import urllib
 import webbrowser
-# export SPOTIPY_CLIENT_ID='db13c5c481574855b69a6209bdffc279'
-# export SPOTIPY_CLIENT_SECRET='ee74200a30754633baff860d4c0546f9'
-# export SPOTIPY_REDIRECT_URI='http://localhost:8888/callback'
+
+cmd("export SPOTIPY_CLIENT_ID='db13c5c481574855b69a6209bdffc279'")
+cmd("export SPOTIPY_CLIENT_SECRET='ee74200a30754633baff860d4c0546f9'")
+cmd("export SPOTIPY_REDIRECT_URI='http://localhost:8888/callback'")
+
 scope = 'user-library-read'
 username="Giannhs Daras"
 try:
@@ -16,7 +18,7 @@ except:
 
 if token:
     sp = spotipy.Spotify(auth=token)
-    results = sp.search(q='mazonakis', limit=20)
+    results = sp.search(q='mazonakis leipei pali o theos', limit=20)
     print(results.keys())
     for i, t in enumerate(results['tracks']['items']):
         print ' ', i, t['name']
