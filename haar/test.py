159f1e77ef462a0fc450c88509b0535897ff5155
John <daras.giannhs@gmail.com>
2017-09-09 21:21:35 +0300

    haar classifier, negatives images, background photos

---
+++ haar/test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+lego_cascade = cv2.CascadeClassifier('classifier/cascade.xml')
+img = cv2.imread('random.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+legos = lego_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in legos:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
