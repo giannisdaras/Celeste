57e801f245a56e79b7cf2d99eca32cc7c94b0565
John <daras.giannhs@gmail.com>
2017-09-11 15:33:14 +0300

    Extended dataset, using dataset generator

--- haar/fastscript.sh
+++ haar/fastscript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-find ./negative_images -iname "*.jpg" > negatives.txt &&
-find ./positive_images -iname "*.jpg" > positives.txt &&
+find ./increased_negatives -iname "*.jpeg" > increased_negatives.txt &&
+find ./increased_positives -iname "*.jpeg" > increased_positives.txt &&
 
 
-perl createsamples.pl positives.txt negatives.txt samples 5000 "opencv_createsamples -bgcolor 0 -bgthresh 0 -maxxangle 1.1 -maxyangle 1.1 maxzangle 0.5 -maxidev 40 -w 40 -h 40" &&
+perl createsamples.pl increased_positives.txt increased_negatives.txt samples 10000 "opencv_createsamples -bgcolor 0 -bgthresh 0 -maxxangle 1.1 -maxyangle 1.1 maxzangle 0.5 -maxidev 40 -w 40 -h 40" &&
 python mergevec.py -v samples -o samples.vec &&
 
-opencv_traincascade -data classifier -vec samples.vec -bg negatives.txt -numStages 20 -minHitRate 0.999 -maxFalseAlarmRate 0.3 -numPos 2500 -numNeg 1200 -w 40 -h 40 -mode ALL -precalcValBBufSize 2048 -featureType LBP
+opencv_traincascade -data increased_lots -vec samples.vec -bg increased_negatives.txt -numStages 10 -minHitRate 0.999 -maxFalseAlarmRate 0.25 -numPos 5000 -numNeg 5000 -w 40 -h 40 -mode ALL -precalcValBBufSize 2048 -featureType LBP
 
--- haar/test.py
+++ haar/test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
-lego_cascade = cv2.CascadeClassifier('classifier1/cascade.xml')
-for i in range(1,11):	
+lego_cascade = cv2.CascadeClassifier('lots/cascade.xml')
+for i in range(1,19):	
 	img = cv2.imread('tests/{0}.jpg'.format(i))
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	legos = lego_cascade.detectMultiScale(gray, 1.3, 5)
